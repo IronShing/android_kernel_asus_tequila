f92beb4990965447dc79dc74e4e40e3e89e20ee2
ComicoX <comico_work@outlook.com>
2022-01-15 01:26:08 +0200

    Remove kernel: module: Hack to force load prebuilt vendor modules

--- kernel/module.c
+++ kernel/module.c
@@ -3705,9 +3705,6 @@ static int load_module(struct load_info *info, const char __user *uargs,
 		goto free_copy;
 	}
 
-	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
-	flags |= MODULE_INIT_IGNORE_VERMAGIC;
-
 	err = module_sig_check(info, flags);
 	if (err)
 		goto free_copy;
